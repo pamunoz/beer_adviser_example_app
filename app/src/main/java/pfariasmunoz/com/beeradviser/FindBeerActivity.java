aa9c0aa54eabca1cd8a16b089d5b38f461cfb0e8
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-25 23:20:02 -0300

    Initial commit

---
+++ app/src/main/java/pfariasmunoz/com/beeradviser/FindBeerActivity.java
@@ -0,0 +1,19 @@
+package pfariasmunoz.com.beeradviser;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+public class FindBeerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    // Call when the user clicks the button
+    public void onClickFindBeer(View view) {
+
+    }
+}
